bd2ba4cc50dd1b2e4f14ffec465da39f89bc626f
Gary Taylor <gary.taylor@hismessages.com>
2024-01-22 09:07:43 +0000

    Initial version

--- lib/commands/full_system_command.rb
+++ lib/commands/full_system_command.rb
@@ -2,11 +2,20 @@ require "docker"
 class FullSystem < ::Rails::Command::Base
   desc "stop [SERVICE]", "Stop a service (web or sidekiq)"
   def stop(service)
-    Docker.url = 'unix:///var/run/docker.sock'
     containers = Docker::Container.all(all: true, filters: JSON.dump(label: ['traefik.http.routers.et1-router.service=et1-service']))
     # For now just accept the first one - we need to be a bit more intelligent though
     container = containers.first
-    exec_instance = container.exec ['invoker', 'remove', service] do |stream_type, stream_content|
+    container.exec ['invoker', 'remove', service] do |stream_type, stream_content|
+      debug = 1
+    end
+  end
+
+  desc "start [SERVICE]", "Start a service (web or sidekiq)"
+  def start(service)
+    containers = Docker::Container.all(all: true, filters: JSON.dump(label: ['traefik.http.routers.et1-router.service=et1-service']))
+    # For now just accept the first one - we need to be a bit more intelligent though
+    container = containers.first
+    container.exec ['invoker', 'add', service] do |stream_type, stream_content|
       debug = 1
     end
   end
