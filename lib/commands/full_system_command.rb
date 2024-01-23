847dbef89113c568a8024dbb19fe78b9bd048213
Gary Taylor <gary.taylor@hismessages.com>
2024-01-23 06:59:37 +0000

    Now can be run in any service with the help of labels in the full system environment

--- lib/commands/full_system_command.rb
+++ lib/commands/full_system_command.rb
@@ -2,9 +2,6 @@ require "docker"
 class FullSystem < ::Rails::Command::Base
   desc "stop [SERVICE]", "Stop a service (web or sidekiq)"
   def stop(service)
-    containers = Docker::Container.all(all: true, filters: JSON.dump(label: ['traefik.http.routers.et1-router.service=et1-service']))
-    # For now just accept the first one - we need to be a bit more intelligent though
-    container = containers.first
     container.exec ['invoker', 'remove', service] do |stream_type, stream_content|
       debug = 1
     end
@@ -12,11 +9,21 @@ class FullSystem < ::Rails::Command::Base
 
   desc "start [SERVICE]", "Start a service (web or sidekiq)"
   def start(service)
-    containers = Docker::Container.all(all: true, filters: JSON.dump(label: ['traefik.http.routers.et1-router.service=et1-service']))
-    # For now just accept the first one - we need to be a bit more intelligent though
-    container = containers.first
     container.exec ['invoker', 'add', service] do |stream_type, stream_content|
       debug = 1
     end
+  end
+
+  private
+
+  def container
+    @container ||= Docker::Container.all(all: true, filters: JSON.dump(label: ["et-full-system.service.source=#{service_root_dir}"])).first
+  end
+
+  def service_root_dir(dir = Dir.pwd)
+    return dir if File.exist?(File.join(dir, 'Gemfile'))
+    return nil if dir == '/'
+
+    service_root_dir(File.dirname(dir))
   end
 end
